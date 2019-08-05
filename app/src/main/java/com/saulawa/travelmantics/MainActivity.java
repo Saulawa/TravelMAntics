c5c724d16999d248609ab2b50a8ece632036c0a4
Saulawa <31113588+Saulawa@users.noreply.github.com>
2019-08-05 22:05:35 +0100

    a
    a

---
+++ app/src/main/java/com/saulawa/travelmantics/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saulawa.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
